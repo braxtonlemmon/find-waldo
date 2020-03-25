87989acfa295166bba4489e2d16b688d7b70f74f
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-25 13:36:12 +0100

    Begin frontend work. Add App component. Display WWaldo image.

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Paragraph from './Paragraph.jsx';
+import Scene from './Scene.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Paragraph />  
+      <Scene />
+    </>
+  );
+}
+
+export default App
---
+++ app/javascript/components/Scene.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoldRushScene from 'images/gold_rush_scene.jpg';
+
+
+
+const Scene = () => {
+  return (
+    <img src={GoldRushScene} alt="Where's Waldo Gold Rush Scene"/>
+  )
+}
+
+export default Scene;
