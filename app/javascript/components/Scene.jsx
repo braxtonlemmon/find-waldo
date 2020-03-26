b12931434a4537cb42720afcdab7af177ead6245
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-26 16:18:24 +0100

    Temporarily add handleMouseMove, used to find coordinates of characters.

--- app/javascript/components/Scene.jsx
+++ app/javascript/components/Scene.jsx
@@ -9,43 +9,21 @@ const Img = styled.img`
   cursor: crosshair;
 `;
 
-const Circle = styled.div`
-  border: 2px solid black;
-  border-radius: 50%;
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  left: ${props => `${props.x}px` };
-  top: ${props => `${props.y}px` };
-`;
+const handleMouseMove = (e) => {
+  const bounds = document.getElementById('waldoScene').getBoundingClientRect();
+  const x = e.clientX - bounds.left;
+  const y = e.clientY - bounds.top;
+  console.log(`x: ${x} y: ${y}`);
+}
 
 const Scene = (props) => {
-  const handleClick = () => {
-    const parent = document.getElementById('waldoScene');
-    const bounds = parent.getBoundingClientRect();
-    const posX = event.clientX - bounds.left;
-    const posY = event.clientY - bounds.top;
-    console.log(posX, posY);
-    drawCircle(posX, posY)
-  }
-
-  const drawCircle = (x, y) => {
-    console.log(x, y);
-    const box = document.querySelector('.scene-box');
-    console.log(box);
-    const circle = document.createElement('div');
-    circle.className = 'circle';
-    circle.style.left = `${x - 15}px`;
-    circle.style.top = `${y - 15}px`;
-    box.appendChild(circle);
-  }
-
   return (
     <div className="scene-box">
       <Img 
         src={GoldRushScene} 
         alt="Where's Waldo Gold Rush Scene"
         onClick={props.handleClick}
+        onMouseMove={handleMouseMove}
         id="waldoScene"
       />
     </div>
