68f566a7284d32c6a95306fe8c09b9ccbd43a71f
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-03-26 16:37:03 +0100

    Create Character model. Seed Character data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+characters = [
+  ['Waldo',  383,  412,  556,  618],
+  ['Wenda',  1183, 1206, 1039, 1081],
+  ['Woof',   1468, 1491, 294,  311],
+  ['Odlaw',  778,  797,  156,  204],
+  ['Wizard', 1883, 1904, 342, 378]
+]
+
+characters.each do |name, left, right, top, bottom|
+  Character.create(
+    name: name,
+    left: left,
+    right: right,
+    top: top,
+    bottom: bottom
+  )
+end
